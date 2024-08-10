69f458de61d7d40becf0aed6ab4fdfd22d500a82
lee gyuseung <gyus0421@gmail.com>
2024-08-10 21:43:36 +0900

    #4.1 Migrations

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import House
+ 
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+  pass
--- houses/models.py
+++ houses/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 class House(models.Model):
   
-
   """Model Definition for Houses"""
   
-  name = models.models.CharField(max_length=140)
-  price = models.models.PositiveIntegerField()
+  name =  models.CharField(max_length=140)
+  price =  models.PositiveIntegerField()
   description = models.TextField()
   address = models.CharField(max_length=140)
