e7c970ce240b0c8d73d0dab66f13f529977a661c
lee gyuseung <gyus0421@gmail.com>
2024-08-13 23:24:08 +0900

    #5.1 Custom Model

--- houses/models.py
+++ houses/models.py
@@ -6,7 +6,7 @@ class House(models.Model):
   """Model Definition for Houses"""
   
   name =  models.CharField(max_length=140)
-  price_per_night =  models.PositiveIntegerField(verbose_name="Price", help_text="Positive Numbers Only")
+  price_per_night =  models.PositiveIntegerField(verbose_name="Price", help_text='Positive Numbers Only')
   description = models.TextField()
   address = models.CharField(max_length=140)
   pets_allowed = models.BooleanField(verbose_name="Pets Allowed?",default=True, help_text="Does this house allow pet?")
