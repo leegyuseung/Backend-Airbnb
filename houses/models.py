afcc79692f582bcc5d70017fec718d733bddbca5
lee gyuseung <gyus0421@gmail.com>
2024-08-18 20:34:59 +0900

    #5.5 Foreign Keys

--- houses/models.py
+++ houses/models.py
@@ -10,6 +10,6 @@ class House(models.Model):
   description = models.TextField()
   address = models.CharField(max_length=140)
   pets_allowed = models.BooleanField(verbose_name="Pets Allowed?",default=True, help_text="Does this house allow pet?")
-
+  owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
   def __str__(self):
     return self.name
