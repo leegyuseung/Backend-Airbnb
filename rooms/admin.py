3ac81d7f95d16fa3deaff1d93f3782b9de2e6167
lee gyuseung <gyus0421@gmail.com>
2024-09-03 17:58:24 +0900

    #7.3 Admin Methods

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,10 +6,10 @@ class RoomAdmin(admin.ModelAdmin):
     "name",
     "price",
     "kind",
+    "total_amenities",
     "owner",
     "created_at",
-    "updated_at",
-  )
+   )
 
   list_filter = (
     "country",
@@ -20,6 +20,9 @@ class RoomAdmin(admin.ModelAdmin):
     "created_at",
     "updated_at",
   )
+
+  def total_amenities(self, room):
+    return room.amenities.count()
 @admin.register(Amenity)
 class AmenityAdmin(admin.ModelAdmin):
   list_display = (
