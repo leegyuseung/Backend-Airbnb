85f59dc1c37b6ca8e9d87548b77c0f64ec9ca7db
lee gyuseung <gyus0421@gmail.com>
2024-09-09 12:59:47 +0900

    #9.5 DoesNotExist

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.see_all_room),
-    path("<int:room_id>", views.see_one_room),
+    path("<int:room_pk>", views.see_one_room),
     # path("<int:room_id>/<str:room_name>", views.see_one_room),
     # path("<str:room_name>", views.see_one_room)
 ]
