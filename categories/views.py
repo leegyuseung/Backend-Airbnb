24db0eb2da545ce302fc6209924aafaf90127da0
lee gyuseung <gyus0421@gmail.com>
2024-09-10 17:35:12 +0900

    #10.3 Serializer

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
- 
+from .models import Category
+from .serializers import CategorySerializer
+
 @api_view()
 def categories(request):
-   return Response({'ok':True})
+   all_categories = Category.objects.all()
+   serializer = CategorySerializer(all_categories, many=True)
+   return Response({'ok':True, 'categories':serializer.data})
