8534d33fa629053bb737a895f0a115c42d7acf98
lee gyuseung <gyus0421@gmail.com>
2024-09-10 14:18:33 +0900

    #10.2 api_view

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from .models import Category
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+ 
+@api_view()
 def categories(request):
-  all_categories = Category.objects.all()
-  return JsonResponse({'ok':True, 'categories':all_categories})
+   return Response({'ok':True})
