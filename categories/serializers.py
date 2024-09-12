c5309f101e95cd0c559d0cc846cde203dd801400
lee gyuseung <gyus0421@gmail.com>
2024-09-12 17:24:26 +0900

    #10.7 update()

--- categories/serializers.py
+++ categories/serializers.py
@@ -8,4 +8,10 @@ class CategorySerializer(serializers.Serializer):
   created_at = serializers.DateTimeField(read_only=True)
 
   def create(self, validated_data):
-   return Category.objects.create(**validated_data)
+    return Category.objects.create(**validated_data)
+  
+  def update(self, instance, validated_data):
+    instance.name = validated_data.get('name', instance.name)
+    instance.kind = validated_data.get('kind', instance.kind)
+    instance.save()
+    return instance
