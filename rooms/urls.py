ba5f7d995a6746887ee6a6fcc35d3ba307e4995e
lee gyuseung <gyus0421@gmail.com>
2024-09-24 11:11:20 +0900

    #11.0 All Amenities

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.see_all_room),
-    path("<int:room_pk>", views.see_one_room),
-    # path("<int:room_id>/<str:room_name>", views.see_one_room),
-    # path("<str:room_name>", views.see_one_room)
+  path("amenities/",views.Amenities.as_view()),
+  path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
