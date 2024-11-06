88c358383784438e6e7bdde111458a584e73192b
leegyuseung <gyus0421@gmail.com>
2024-11-06 17:22:07 +0900

    #12.2 Change Password

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.Users.as_view()),
     path('me', views.Me.as_view()),
+    path('change-password',views.ChangePassword.as_view()),
+    path('@<str:username>',views.PublicUser.as_view()), # @를 쓰는이유는 me라는 이름을 가진 사람이 있을 수 있기때문에.
 ]
