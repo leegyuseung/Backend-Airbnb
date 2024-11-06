4252d62c0fc462aa25fb2d95b8d8d015a66f948a
leegyuseung <gyus0421@gmail.com>
2024-11-06 17:31:08 +0900

    #12.3 Log In and Log Out

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("",views.Users.as_view()),
     path('me', views.Me.as_view()),
     path('change-password',views.ChangePassword.as_view()),
+    path('log-in', views.LogIn.as_view()),
+    path('log-out', views.LogOut.as_view()),
     path('@<str:username>',views.PublicUser.as_view()), # @를 쓰는이유는 me라는 이름을 가진 사람이 있을 수 있기때문에.
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError,NotFound
+from users.models import User
 from . import serializers
-from .models import User
 
 class Me(APIView):
   # private url일거고, 내 프로필이랑 공개되지 않은 정보를 받아볼 예정
@@ -71,3 +72,25 @@ class ChangePassword(APIView):
       return Response(status=status.HTTP_200_OK)
     else:
       return Response(status= status.HTTP_400_BAD_REQUEST)
+    
+class LogIn(APIView):
+
+  def post(self, request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    if not username or not password:
+      raise ParseError
+    user = authenticate(request, username=username, password=password)
+    if user:
+      login(request, user)
+      return Response({"ok":"Welcome !"})
+    else:
+      return Response({"error":"wrong password"})      
+    
+class LogOut(APIView):
+
+  permission_classes = [IsAuthenticated]
+
+  def post(self, request):
+    logout(request)
+    return Response({'ok':'bye!'})
