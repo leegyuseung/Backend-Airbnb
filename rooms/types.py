f2ab1c8ac46319b5c7a692c9c9c46dddcb4ca0a7
leegyuseung <gyus0421@gmail.com>
2024-11-12 16:02:50 +0900

    #14.6 Type Relationships

--- rooms/types.py
+++ rooms/types.py
@@ -1,9 +1,11 @@
 import strawberry
 from strawberry import auto
 from . import models 
+from users.types import UserType
 
 @strawberry.django.type(models.Room)
 class RoomType:
   id:auto
   name: auto # auto는 자동으로 model로 가서 type를 알아낸다
   kind: auto
+  owner: 'UserType'
