6bd2a25f62f08b65b04a8496649affcd2e2d67ac
leegyuseung <gyus0421@gmail.com>
2025-01-03 22:12:33 +0900

    #20.11 Kakao Log In

--- users/urls.py
+++ users/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('token-login', obtain_auth_token),
     path('jwt-login', views.JWTLogIn.as_view()),
     path('github',views.GithubLogIn.as_view()),
+    path('kakao',views.KakaoLogin.as_view()),
     path('@<str:username>',views.PublicUser.as_view()), # @를 쓰는이유는 me라는 이름을 가진 사람이 있을 수 있기때문에.
 
 ]
