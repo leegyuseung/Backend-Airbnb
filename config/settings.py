5a593c6eebe1044034178046bbd81b4c5f576997
leegyuseung <gyus0421@gmail.com>
2025-01-17 14:40:37 +0900

    #21.6 One Time Upload

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,8 @@ environ.Env.read_env(os.path.join(BASE_DIR,".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 GH_SECRET = env('GH_SECRET')
+CF_ID = env('CF_ID')
+CF_TOKEN = env('CF_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- medias/urls.py
+++ medias/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import photoDetail
+from .views import photoDetail, GetUploadURL
 
 urlpatterns = [
-  path('photo/<int:pk>', photoDetail.as_view()),
+  path('photos/get-url', GetUploadURL.as_view()),
+  path('photos/<int:pk>', photoDetail.as_view()),
 ]
